ce7640bf726038a7a115876122d3c608421392fb
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-09-07 11:54:48 -0500

    Working on auth

--- coffeepot/chemex/urls.py
+++ coffeepot/chemex/urls.py
@@ -1,9 +1,12 @@
-from django.conf.urls import url
 from django.urls import path
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('brew_calculator/', views.BrewCalculatorView.as_view(), name='brew_calculator'),
     path('coffee_tracker/', views.TrackerView.as_view(), name='coffee_tracker'),
     path('coffee_tracker/favorites', views.FavoriteRoasterView.as_view(), name='favorite_roaster'),
